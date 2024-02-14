515435153d4ddfa8b8ca7b3f7fe49e339810b0e6
michael <mihails.levenciks@gmail.com>
2024-02-14 23:58:00 +0200

    app update

--- trading_middleware/__init__.py
+++ trading_middleware/__init__.py
@@ -10,13 +10,6 @@ app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'trading_middleware.sqlite'),
 )
 
-if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
-
 # ensure the instance folder exists
 try:
     os.makedirs(app.instance_path)
